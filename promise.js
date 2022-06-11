e3068b47e210db0ffa154da9915f5e7b1a7c48db
ahsantawil <ahsan.tawil07@gmail.com>
2022-06-12 00:39:28 +0700

    challenge 07

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const [IXX, VGC] = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    
+    const result = [...IXX, ...VGC]
+
+    return result?.filter(item=> item.hasil === emosi)?.length || 0
+  } catch (err) {
+    console.log(`error ${err}`);
+  }
+};
 
 module.exports = {
   promiseOutput,
